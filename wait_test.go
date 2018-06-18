fdcff51183495358f5788eadfe311fc8c8849dd3
xcdb <xcdb@users.noreply.github.com>
2018-06-18 12:24:10 +0100

    more tests

--- wait_test.go
+++ wait_test.go
@@ -1,6 +1,7 @@
 package syncx_test
 
 import (
+	"context"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -53,4 +54,66 @@ func TestWaitAll(t *testing.T) {
 	}
 	e2.Signal()
 	<-step //2
+}
+
+func TestWaitAny_panics(t *testing.T) {
+	assert.Panics(t, func() { syncx.WaitAny() })
+	assert.Panics(t, func() { syncx.WaitAnyContext(context.Background()) })
+
+	ws := make([]syncx.WaitHandle, 9, 9)
+	for i := 0; i < len(ws); i++ {
+		ws[i] = syncx.NewAutoResetEvent(false)
+	}
+	assert.Panics(t, func() { syncx.WaitAny(ws...) })
+	assert.Panics(t, func() { syncx.WaitAnyContext(context.Background(), ws...) })
+}
+
+func TestWaitAny_returnsIndexThatSatisfiedWait(t *testing.T) {
+	for l := 1; l <= 8; l++ {
+		es := make([]*syncx.AutoResetEvent, l, l)
+		ws := make([]syncx.WaitHandle, l, l)
+		for i := 0; i < l; i++ {
+			es[i] = syncx.NewAutoResetEvent(false)
+			ws[i] = es[i]
+		}
+		for j := 0; j < l; j++ {
+			es[j].Signal()
+			ix := syncx.WaitAny(ws...)
+			assert.Equal(t, j, ix)
+		}
+	}
+}
+
+func TestWaitAnyContext_returnsIndexThatSatisfiedWait(t *testing.T) {
+	for l := 1; l <= 8; l++ {
+		es := make([]*syncx.AutoResetEvent, l, l)
+		ws := make([]syncx.WaitHandle, l, l)
+		for i := 0; i < l; i++ {
+			es[i] = syncx.NewAutoResetEvent(false)
+			ws[i] = es[i]
+		}
+		for j := 0; j < l; j++ {
+			es[j].Signal()
+			ix, err := syncx.WaitAnyContext(context.Background(), ws...)
+			assert.Equal(t, j, ix)
+			assert.Nil(t, err)
+		}
+	}
+}
+
+func TestWaitAnyContext_returnsNegative1AndCtxErrWhenCancelled(t *testing.T) {
+	for l := 1; l <= 8; l++ {
+		es := make([]*syncx.AutoResetEvent, l, l)
+		ws := make([]syncx.WaitHandle, l, l)
+		for i := 0; i < l; i++ {
+			es[i] = syncx.NewAutoResetEvent(false)
+			ws[i] = es[i]
+		}
+		ctx, cancel := context.WithCancel(context.Background())
+		cancel()
+		ix, err := syncx.WaitAnyContext(ctx, ws...)
+		assert.Equal(t, -1, ix)
+		assert.NotNil(t, err)
+		assert.Equal(t, ctx.Err(), err)
+	}
 }
