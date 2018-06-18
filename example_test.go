808f90f8c4500de4199eef147d5b20d11e215fca
xcdb <xcdb@users.noreply.github.com>
2018-06-18 09:39:42 +0100

    improved examples

--- example_test.go
+++ example_test.go
@@ -3,6 +3,7 @@ package syncx_test
 import (
 	"fmt"
 	"strings"
+	"sync"
 
 	"github.com/xcdb/syncx"
 )
@@ -60,20 +61,20 @@ func ExampleManualResetEvent() {
 func ExampleBarrier() {
 	b := syncx.NewBarrier(3, func() { fmt.Print(" ") })
 
-	done := make(chan bool, 3)
+	var wg sync.WaitGroup
+	wg.Add(3)
+
 	for i := 1; i <= 3; i++ {
 		go func() {
 			for _, c := range strings.Split("ABCDE", "") {
 				fmt.Print(c)
 				b.SignalAndWait()
 			}
-			done <- true
+			wg.Done()
 		}()
 	}
 
-	for i := 1; i <= 3; i++ {
-		<-done
-	}
+	wg.Wait()
 
 	// Output:
 	// AAA BBB CCC DDD EEE
@@ -118,6 +119,113 @@ func ExampleSemaphore() {
 			s.Release()
 		}()
 	}
+
+	//...
+
+}
+
+func ExampleWaitAny() {
+	e1 := syncx.NewAutoResetEvent(false)
+	e2 := syncx.NewManualResetEvent(false)
+
+	//start a bunch of goroutines
+	for i := 1; i <= 5; i++ {
+		go func() {
+			//...
+			syncx.WaitAny(e1, e2)
+			//...
+		}()
+	}
+
+	//signal the AutoResetevent, releasing a single goroutine
+	e1.Signal()
+
+	//release another
+	e1.Signal()
+
+	//signal the ManualResetEvent, releasing the remaining goroutines
+	e2.Signal()
+
+	//...
+
+}
+
+func ExampleWaitAny_returnValue() {
+	e := syncx.NewManualResetEvent(false)
+	s := syncx.NewSemaphore(3)
+
+	//start a bunch of goroutines
+	//initially, only 3 can make progress at a time
+	for i := 1; i <= 5; i++ {
+		go func() {
+			//...
+			ix := syncx.WaitAny(e, s)
+			//...
+
+			//if we consumed a resource from the semaphone, we need to release it
+			if ix == 1 {
+				s.Release()
+			}
+		}()
+	}
+
+	//signal the event, releasing all remaining goroutines
+	e.Signal()
+
+	//...
+
+}
+
+func ExampleWaitAll() {
+	e1 := syncx.NewAutoResetEvent(false)
+	e2 := syncx.NewManualResetEvent(false)
+	s := syncx.NewSemaphore(3)
+
+	//start a bunch of goroutines
+	for i := 1; i <= 5; i++ {
+		go func() {
+			//...
+			syncx.WaitAll(e1, e2, s)
+			//...
+			s.Release()
+		}()
+	}
+
+	//signal the AutoResetEvent (nothing is released as all 3 waits are required)
+	e1.Signal()
+
+	//signal the ManualResetEvent, releasing a single goroutine
+	e2.Signal()
+
+	//signal the AutoResetEvent, releasing another goroutine
+	e1.Signal()
+
+	//...
+
+}
+
+func ExampleWaitAll_semantics() {
+	e1 := syncx.NewAutoResetEvent(false)
+	e2 := syncx.NewManualResetEvent(false)
+
+	//start a bunch of goroutines
+	for i := 1; i <= 5; i++ {
+		go func() {
+			//...
+			syncx.WaitAll(e1, e2)
+			//...
+		}()
+	}
+
+	//signal the ManualResetEvent
+	e2.Signal()
+
+	//set the ManualResetEvent to non-signalled
+	e2.Reset()
+
+	//as the current implementation semantics are 'all have been signalled' this still releases a goroutine
+	//this behaviour should not be depended upon
+	e1.Signal()
 
 	//...
 
